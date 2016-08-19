52621920a30aef8efbebb6b6883908069b38ca86
Kathryn Hodge <kahodge@vassar.edu>
2016-08-19 13:07:38 -0500

    Sure

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', function (req, res) {
         }
         if (event.postback) {
         	let text = JSON.stringify(event.postback)
-        	sendTextMessage(sender, "Postback")
+        	decideMessage(sender, text);
         	continue
       }
     }
