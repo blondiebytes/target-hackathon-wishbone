866ff52c9c62f73f5ea966a3f40ba2f9641ea149
Kathryn Hodge <kahodge@vassar.edu>
2016-08-20 16:36:06 -0500

    Cleaning code

--- index.js
+++ index.js
@@ -47,34 +47,109 @@ app.post('/webhook/', function (req, res) {
 
 const token = "EAADPuY7AusgBAL5wNKMrokW32BgDCVESDwtXp0u4ytJ0gmxHB9LISBwGW2y6XFEi3oBgNFb0BqMbV89d80DUqfZBV4Ln4ttZBsKOg1alqDqaNA1Wlx4ZAFQOZBAj7iquLpmTcEEcDs6FriESRU0WTQ99TAECWB4UNWArcqGIhAZDZD"
 
+var popularTypesOfItems = {
+    invitations:  {
+        header: "Invitations",
+        subtitle: "Friends and Family Welcome!",
+        image_url: "http://scene7.targetimg1.com/is/image/Target/15406466?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+        url: "http://www.target.com/c/baby-shower-party-supplies/-/N-4soei%7Cd_item_type_all%3Ainvitation%20packs?Sort=Featured&clearCategId=4soei&Nao=0",
+        button_title: "See Items Like"
+    },
+    favorBags: {
+        header: "Favor Bags",
+        subtitle: "Grab-and-go sweet treats",
+        image_url: "http://scene7.targetimg1.com/is/image/Target/50701956?wid=450&hei=450&fmt=pjpeg",
+        url: "http://www.target.com/c/baby-shower-party-supplies/-/N-4soei%7Cd_item_type_all%3AFavor%20Bag?Sort=Featured&clearCategId=4soei&Nao=0",
+        button_title: "See Items Like"
+    }
+}
+
+var specificItems = {
+    sunscreens: {
+        sunscreen1: {
+            header: "Alba Botanica",
+            subtitle: "Keeping your skin youthful",
+            image_url: "http://scene7.targetimg1.com/is/image/Target/16872833?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+            url: "http://www.target.com/p/alba-botanica-emollient-sunscreen-active-kids-clear-spray-spf-50-6-oz/-/A-16872833",
+            button_title_1: "See Item",
+            button_title_2: "Add to Cart"
+        },
+         sunscreen2: {
+            header: "Neutrogena Beach Defense",
+            subtitle: "Protecting your skin on the go!",
+            image_url: "http://scene7.targetimg1.com/is/image/Target/50787513?wid=450&hei=450&fmt=pjpeg",
+            url: "http://www.target.com/p/neutrogena-oh-joy-beach-defense-spray-sunscreen-broad-spectrum-spf-70-6-5-oz/-/A-50787513",
+            button_title_1: "See Item",
+            button_title_2: "Add to Cart"
+        },
+        sunscreen3: {
+            header: "Coppertone Kids",
+            subtitle: "Sun protection made easy!",
+            image_url: "http://scene7.targetimg1.com/is/image/Target/50584700?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+            url: "http://www.target.com/p/coppertone-kids-sunscreen-continuous-spray-spf-50/-/A-50584700",
+            button_title_1: "See Item",
+            button_title_2: "Add to Cart"
+        }
+    },
+
+    infant_boy_clothing: {
+         clothing1: {
+            header: "Organically-Made Pajamas",
+            subtitle: "Keeping your baby comfy throughout the night",
+            image_url: "http://target.scene7.com/is/image/Target/51186963?wid=450&hei=450&fmt=pjpeg",
+            url: "http://www.target.com/p/lamaze-baby-waffle-footed-sleeper-red/-/A-51261899",
+            button_title_1: "See Item",
+            button_title_2: "Add to Cart"
+        },
+        clothing2: {
+            header: "Baby Boys' 4 Piece: Cat & Jack",
+            subtitle: "Providing style and comfort",
+            image_url: "http://target.scene7.com/is/image/Target/50897885?wid=450&hei=450&fmt=pjpeg",
+            url: "http://www.target.com/p/baby-boys-4-piece-tiger-set-baby-cat-jack-turquoise-white/-/A-51112241",
+            button_title_1: "See Item",
+            button_title_2: "Add to Cart"
+        },
+        clothing3: {
+            header: "Burt's Bees Infant 2 Pack",
+            subtitle: "Ready to rock and roll!",
+            image_url: "http://target.scene7.com/is/image/Target/50363846?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+            url: "http://www.target.com/p/burt-s-bees-baby-infant-boys-2-pack-bodysuit-camo-striped/-/A-50363847",
+            button_title_1: "See Item",
+            button_title_2: "Add to Cart"
+        }
+
+    } 
+}
+
+
 function decideMessage(sender, text1) {
 	let text = text1.toLowerCase();
 	if (text.includes("baby shower") && text.includes("throwing")) {
 		sendTextMessage(sender, "Congrats! Do you need anything for your baby shower?");
-		sendGenericMessage1(sender)
-	} else if (text.includes("add to shopping cart")) {
+		send2TypesOfItems(sender, popularTypesOfItems.invitations, popularTypesOfItems.favorBags)
+	} else if (text.includes("add to cart")) {
 		sendTextMessage(sender, "Added to shopping cart!")
 	} else if (text.includes("no")) {
-		sendButtonMessage(sender, "I see it's summertime. Here are some things you might need.")
+		sendButtonMessage(sender, "I see it's summertime. Here are some things you might need.", "Sunscreen", "Flip Flops", "Sunglasses")
 	} else if (text.includes("sunscreen")) {
 		sendTextMessage(sender, "Here are some great deals on sunscreen!");
-		sendGenericMessage2(sender);
+	    send3SpecificItems(sender, specificItems.sunscreen.sunscreen1, specificItems.sunscreen.sunscreen2, specificItems.sunscreen.sunscreen3);
 	} else if (text.includes("gift")) {
-		sendButtonMessage2(sender, "What is the occasion?");
+		sendButtonMessage(sender, "What is the occasion?", "Baby Shower", "Birthday", "Graduation");
 	} else if (text.includes("baby shower")) {
-		sendButtonMessage3(sender, "What's your price range?")
-	} else if (text.includes("$15 - $50")) {
-		sendButtonMessage4(sender, "Here are some customer favorites!")
+		sendButtonMessage(sender, "What's your price range?", "$0 - $15", "$15 - $50", "$50 - $100")
+	} else if (text.includes("$")) {
+		sendButtonMessage(sender, "Here are some customer favorites!", "Clothes", "Stroller", "Toys")
 	} else if (text.includes("clothes")) {
 		sendTextMessage(sender, "Boy or girl?")
 	} else if (text.includes("boy")) {
-		sendGenericMessage3(sender);
+		send3SpecificItems(sender, specificItems.infant_boy_clothing.clothing1, specificItems.infant_boy_clothing.clothing2, specificItems.infant_boy_clothing.clothing3)
 	} else {
 		sendTextMessage(sender, "Can you ask again?");
 	}
 }
 
-function sendButtonMessage(sender, text) {
+function sendButtonMessage(sender, text, button1, button2, button3) {
 	let messageData = {
     "attachment":{
       "type":"template",
@@ -84,194 +159,18 @@ function sendButtonMessage(sender, text) {
         "buttons":[
           {
             "type":"postback",
-            "title":"Sunscreen",
-            "payload":"sunscreen"
+            "title":button1,
+            "payload":button1.toLowerCase()
           },
           {
             "type":"postback",
-            "title":"Flip Flops",
-            "payload":"flip flops"
+            "title":button2,
+            "payload":button2.toLowerCase()
           },
           {
             "type":"postback",
-            "title":"Sunglasses",
-            "payload":"sunglasses"
-          }
-        ]
-      }
-    } 
-}
-     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function sendButtonMessage2(sender, text) {
-	let messageData = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":text,
-        "buttons":[
-          {
-            "type":"postback",
-            "title":"Baby Shower",
-            "payload":"baby shower"
-          },
-          {
-            "type":"postback",
-            "title":"Birthday",
-            "payload":"birthday"
-          },
-          {
-            "type":"postback",
-            "title":"Graduation",
-            "payload":"graduation"
-          }
-        ]
-      }
-    } 
-}
-     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function sendButtonMessage3(sender, text) {
-	let messageData = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":text,
-        "buttons":[
-          {
-            "type":"postback",
-            "title":"$0 - $15",
-            "payload":"$0 - $15"
-          },
-          {
-            "type":"postback",
-            "title":"$15 - $50",
-            "payload":"$15 - $50"
-          },
-          {
-            "type":"postback",
-            "title":"$50 - $100",
-            "payload":"$50 - $100"
-          }
-        ]
-      }
-    } 
-}
-     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function sendButtonMessage4(sender, text) {
-	let messageData = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":text,
-        "buttons":[
-          {
-            "type":"postback",
-            "title":"Clothes",
-            "payload":"clothes"
-          },
-          {
-            "type":"postback",
-            "title":"Stroller",
-            "payload":"stroller"
-          },
-          {
-            "type":"postback",
-            "title":"Toys",
-            "payload":"toys"
-          }
-        ]
-      }
-    } 
-}
-     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function sendButtonMessage5(sender, text) {
-	let messageData = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":text,
-        "buttons":[
-          {
-            "type":"postback",
-            "title":"Shoes",
-            "payload":"shoes"
-          },
-          {
-            "type":"postback",
-            "title":"Clothes",
-            "payload":"clothes"
-          },
-          {
-            "type":"postback",
-            "title":"Toys",
-            "payload":"toys"
+            "title":button3,
+            "payload":button3.toLowerCase()
           }
         ]
       }
@@ -314,29 +213,29 @@ function sendTextMessage(sender, text) {
     })
 }
 
-function sendGenericMessage1(sender) {
+function send2TypesOfItems(sender, obj1, obj2) {
     let messageData = {
         "attachment": {
             "type": "template",
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Invitations",
-                    "subtitle": "Friends and Family Welcome!",
-                    "image_url": "http://scene7.targetimg1.com/is/image/Target/15406466?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+                    "title": obj1.header,
+                    "subtitle": obj1.subtitle,
+                    "image_url": obj1.image_url,
                     "buttons": [{
                         "type": "web_url",
-                        "url": "http://www.target.com/c/baby-shower-party-supplies/-/N-4soei%7Cd_item_type_all%3Ainvitation%20packs?Sort=Featured&clearCategId=4soei&Nao=0",
-                        "title": "See Items Like"
+                        "url": obj1.url,
+                        "title": obj1.button_title
                     }],
                 }, {
-                    "title": "Favor Bags",
-                    "subtitle": "Grab-and-go sweet treats",
-                    "image_url": "http://scene7.targetimg1.com/is/image/Target/50701956?wid=450&hei=450&fmt=pjpeg",
+                    "title": obj2.header,
+                    "subtitle": obj2.subtitle,
+                    "image_url": obj2.image_url,
                     "buttons": [{
                         "type": "web_url",
-                        "url": "http://www.target.com/c/baby-shower-party-supplies/-/N-4soei%7Cd_item_type_all%3AFavor%20Bag?Sort=Featured&clearCategId=4soei&Nao=0",
-                        "title": "See Items Like",
+                        "url": obj2.url,
+                        "title": obj2.button_title
                     }],
                 }]
             }
@@ -359,50 +258,50 @@ function sendGenericMessage1(sender) {
     })
 }
 
-function sendGenericMessage2(sender) {
+function send3SpecificItems(sender, obj1, obj2, obj3) {
     let messageData = {
         "attachment": {
             "type": "template",
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Alba Botanica",
-                    "subtitle": "Keeping your skin youthful",
-                    "image_url": "http://scene7.targetimg1.com/is/image/Target/16872833?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+                    "title": obj1.header,
+                    "subtitle": obj1.subtitle,
+                    "image_url": obj1.image_url,
                     "buttons": [{
                         "type": "web_url",
-                        "url": "http://www.target.com/p/alba-botanica-emollient-sunscreen-active-kids-clear-spray-spf-50-6-oz/-/A-16872833",
-                        "title": "See Item"
+                        "url": obj1.url,
+                        "title": obj1.button_title_1
                     }, {
                         "type": "postback",
-                        "title": "Add to Cart",
-                        "payload": "add to shopping cart",
+                        "title": obj1.button_title_2,
+                        "payload": obj1.button_title_2.toLowerCase(),
                     }],
                 }, {
-                    "title": "Neutrogena Beach Defense",
-                    "subtitle": "Protecting your skin on the go!",
-                    "image_url": "http://scene7.targetimg1.com/is/image/Target/50787513?wid=450&hei=450&fmt=pjpeg",
+                    "title": obj2.header,
+                    "subtitle": obj2.subtitle,
+                    "image_url": obj2.image_url,
                     "buttons": [{
                         "type": "web_url",
-                        "url": "http://www.target.com/p/neutrogena-oh-joy-beach-defense-spray-sunscreen-broad-spectrum-spf-70-6-5-oz/-/A-50787513",
-                        "title": "See Item",
+                        "url": obj2.url,
+                        "title": obj2.button_title_1,
                     }, {
                     	"type": "postback",
-                        "title": "Add to Cart",
-                        "payload": "add to shopping cart",
+                        "title": obj2.button_title_2,
+                        "payload": obj2.button_title_2.toLowerCase(),
                     }],
                 }, {
-                    "title": "Coppertone Kids",
-                    "subtitle": "Sun protection made easy!",
-                    "image_url": "http://scene7.targetimg1.com/is/image/Target/50584700?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
+                    "title": obj3.header,
+                    "subtitle": obj3.subtitle,
+                    "image_url": obj3.image_url,
                     "buttons": [{
                         "type": "web_url",
-                        "url": "http://www.target.com/p/coppertone-kids-sunscreen-continuous-spray-spf-50/-/A-50584700",
-                        "title": "See Item",
+                        "url": obj3.url,
+                        "title": obj3.button_title_1,
                     }, {
                     	"type": "postback",
-                        "title": "Add to Cart",
-                        "payload": "add to shopping cart",
+                        "title": obj3.button_title_2,
+                        "payload": obj3.button_title_2.toLowerCase(),
                     }],
                 }]
             }
@@ -424,74 +323,6 @@ function sendGenericMessage2(sender) {
         }
     })
 }
-
-function sendGenericMessage3(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Organically-Made Pajamas",
-                    "subtitle": "Keeping your baby comfy throughout the night",
-                    "image_url": "http://target.scene7.com/is/image/Target/51186963?wid=450&hei=450&fmt=pjpeg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "http://www.target.com/p/lamaze-baby-waffle-footed-sleeper-red/-/A-51261899",
-                        "title": "See Item"
-                    }, {
-                        "type": "postback",
-                        "title": "Add to Cart",
-                        "payload": "add to shopping cart",
-                    }],
-                }, {
-                    "title": "Baby Boys' 4 Piece: Cat & Jack",
-                    "subtitle": "Providing style and comfort",
-                    "image_url": "http://target.scene7.com/is/image/Target/50897885?wid=450&hei=450&fmt=pjpeg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "http://www.target.com/p/baby-boys-4-piece-tiger-set-baby-cat-jack-turquoise-white/-/A-51112241",
-                        "title": "See Item",
-                    }, {
-                    	"type": "postback",
-                        "title": "Add to Cart",
-                        "payload": "add to shopping cart",
-                    }],
-                }, {
-                    "title": "Burt's Bees Infant 2 Pack",
-                    "subtitle": "Ready to rock and roll!",
-                    "image_url": "http://target.scene7.com/is/image/Target/50363846?wid=1024&hei=1024&qlt=70&fmt=pjpeg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "http://www.target.com/p/burt-s-bees-baby-infant-boys-2-pack-bodysuit-camo-striped/-/A-50363847",
-                        "title": "See Item",
-                    }, {
-                    	"type": "postback",
-                        "title": "Add to Cart",
-                        "payload": "add to shopping cart",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-
 
 
 // Spin up the server
